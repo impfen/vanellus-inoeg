e4158650c46405856e6ceddce5b8a4c3794bb196
Benjamin Bender <bb@codepoet.de>
2022-01-09 19:47:42 +0100

    fix: typings/imports

--- src/api/UserApi.ts
+++ src/api/UserApi.ts
@@ -7,6 +7,7 @@ import { AnonymousApiInterface } from "./AnonymousApiInterface";
 import {
     BookingData,
     ContactData,
+    EncryptedBackup,
     QueueToken,
     UserKeyPairs,
 } from "./interfaces";
